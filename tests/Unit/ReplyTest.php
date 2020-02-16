95914c1381e08424dceed77b9c1cfd9c4de86c84
Bernard Muchira <muchirasamwel@gmail.com>
2020-02-16 20:56:03 +0300

    Episode 59 Wrapping username in A reply with anchor to user profile and tests for the same

--- tests/Unit/ReplyTest.php
+++ tests/Unit/ReplyTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Reply;
 use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
@@ -29,10 +30,23 @@ class ReplyTest extends TestCase
 
     public function test_reply_can_detect_all_mentioned_users_in_the_body()
     {
-        $reply = create('App\Reply', [
+        $reply = new Reply([
             'body' => '@JaneDoe wants to talk to @JohnDoe'
         ]);
 
         $this->assertEquals(['JaneDoe', 'JohnDoe'], $reply->mentionedUsers());
+    }
+
+    public function tests_mentioned_username_in_the_body_is_wrapped_within_anchor_tags()
+    {
+        $reply = new Reply([
+            'body' => 'Hello @Jane-Doe.'
+        ]);
+
+        $this->assertEquals(
+            'Hello <a href="/profiles/Jane-Doe">@Jane-Doe</a>.',
+            $reply->body
+        );
+
     }
 }
